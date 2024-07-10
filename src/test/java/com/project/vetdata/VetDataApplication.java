b947920bd353b06e1da0e8aa12afa54af70fd298
Cleyton <cleytonjosedesouza@hotmail.com>
2024-07-10 13:44:54 -0300

    folder organization

---
+++ src/test/java/com/project/vetdata/VetDataApplication.java
@@ -0,0 +1,13 @@
+package com.project.vetdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetDataApplication.class, args);
+	}
+
+}
