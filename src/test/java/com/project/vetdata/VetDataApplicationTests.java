d6824d4198feea1fe7fb95bce2012e7a808da196
Cleyton <cleytonjosedesouza@hotmail.com>
2024-07-10 11:24:59 -0300

    first commit

---
+++ src/test/java/com/project/vetdata/VetDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.vetdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
