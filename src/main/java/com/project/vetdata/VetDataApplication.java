25d8f6f4b9afa29fe815e481038e8053a693ecc4
Cleyton <cleytonjosedesouza@hotmail.com>
2024-07-17 11:08:11 -0300

    Implementing JPAConfig

--- src/main/java/com/project/vetdata/VetDataApplication.java
+++ src/main/java/com/project/vetdata/VetDataApplication.java
@@ -2,16 +2,11 @@ package com.project.vetdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.project.vetdata.model")
-@EnableJpaRepositories(basePackages = "com.project.vetdata.repository")
 public class VetDataApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VetDataApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(VetDataApplication.class, args);
 	}
 
 }
