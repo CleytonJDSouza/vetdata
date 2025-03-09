fc3d7bfd20e32600b9b7dbfa5e81d12c1b211fb1
Felipe Santaniello <felipe_sbc_sp@hotmail.com>
2025-03-09 20:19:05 -0300

    Add update form and breed list interfaces

--- src/main/java/com/project/vetdata/controller/DogBreedMVController.java
+++ src/main/java/com/project/vetdata/controller/DogBreedMVController.java
@@ -1,7 +1,9 @@
 package com.project.vetdata.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,4 +14,17 @@ public class DogBreedMVController {
     public String form(){
         return "breed_form";
     }
+
+    @GetMapping("form_update/{id}")
+    public String formUpdate(@PathVariable Long id, Model model){
+
+        model.addAttribute("id", id);
+        return "breed_form_update";
+    }
+
+    @GetMapping("list")
+    public String list(){
+        return "breed_list";
+    }
+
 }
