4569260f634b1566afc8650457e6fe3908b96a32
Cleyton <cleytonjosedesouza@hotmail.com>
2025-04-17 17:08:10 -0300

    List Health Record

--- src/main/java/com/project/vetdata/repository/HealthRecordRepository.java
+++ src/main/java/com/project/vetdata/repository/HealthRecordRepository.java
@@ -1,10 +1,20 @@
 package com.project.vetdata.repository;
 
 import com.project.vetdata.model.HealthRecord;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HealthRecordRepository extends JpaRepository<HealthRecord, Long> {
+
+    @Query(value = "SELECT * FROM health_records WHERE LOWER(patient) LIKE CONCAT('%', LOWER(:search), '%')",
+            countQuery = "SELECT COUNT(*) FROM health_records WHERE LOWER(patient) LIKE CONCAT('%', LOWER(:search), '%')",
+            nativeQuery = true
+    )
+    Page<HealthRecord> searchByPatient(@Param("search") String search, Pageable pageable);
 
 }
--- src/main/java/com/project/vetdata/service/HealthRecordService.java
+++ src/main/java/com/project/vetdata/service/HealthRecordService.java
@@ -3,8 +3,12 @@ package com.project.vetdata.service;
 import com.project.vetdata.dto.HealthRecordCreateDTO;
 import com.project.vetdata.dto.HealthRecordUpdateDTO;
 import com.project.vetdata.model.HealthRecord;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface HealthRecordService {
     HealthRecord createHealthRecord(HealthRecordCreateDTO dto);
     HealthRecord updateHealthRecord(Long id, HealthRecordUpdateDTO dto);
+    Page<HealthRecord> findAll(Pageable pageable);
+    Page<HealthRecord> searchByPatient(String search, Pageable pageable);
 }
