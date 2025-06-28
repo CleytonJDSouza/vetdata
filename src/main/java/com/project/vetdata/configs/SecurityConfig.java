7acca7ef6ac54e251a70810b5931de3447dea2fe
Felipe Santaniello <felipe_sbc_sp@hotmail.com>
2025-06-28 11:38:37 -0300

    Adjust request matchers to include static folders

--- src/main/java/com/project/vetdata/configs/SecurityConfig.java
+++ src/main/java/com/project/vetdata/configs/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/css/**", "/js/**", "/img/**", "/webjars/**").permitAll()
+                        .requestMatchers("/ajax/**","/data/**","/fonts/**","/sound/**","/xml.gmap/**","/css/**", "/js/**", "/img/**", "/webjars/**").permitAll()
                         .requestMatchers(
                                 "/",
                                 "/login",
